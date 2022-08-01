99254f5a2319bad822360aa78835ec7f42ad1d4e
杨爽 <yangshuang@douyu.tv>
2022-08-01 21:09:39 +0800

    add tpl create tool

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // vscode这个包，包含了里面所有的api
 import * as vscode from 'vscode';
 import * as GenerateH5 from './h5-ts/generate';
+
 // 在插件被激活的时候，这个方法会被调用
 function activate(context) {
   vscode.commands.registerCommand('Generate.component.h5.ts', GenerateH5.generate);
